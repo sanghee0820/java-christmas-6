ac8cc39120b7c75d953865fd715ed72bba7dc6a9
SangHee Lee <lsh277604@gmail.com>
2023-11-13 23:37:46 +0900

    Refactor: refact InputValidator

--- src/main/java/christmas/app/AppConsole.java
+++ src/main/java/christmas/app/AppConsole.java
@@ -5,15 +5,30 @@ import christmas.controller.ChristmasController;
 public class AppConsole {
     private final InputView inputView;
     private final OutputView outputView;
+
+    private final InputValidator inputValidator;
     private final ChristmasController christmasController;
 
     public AppConsole() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
+        this.inputValidator = new InputValidator();
         this.christmasController = new ChristmasController();
     }
 
-    public void enterVisitDay() {
-        int day = inputView.enterVisitDay();
+    public int enterVisitDay() {
+        while (true) {
+            String dayInput = inputView.enterVisitDay();
+            try {
+                int day = inputValidator.validateSingleNumber(dayInput);
+                return inputValidator.validateDayInRange(day);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage() + "\n");
+            }
+        }
+    }
+
+    public void run(){
+        int day = enterVisitDay();
     }
 }
--- src/main/java/christmas/app/InputView.java
+++ src/main/java/christmas/app/InputView.java
@@ -4,26 +4,14 @@ import camp.nextstep.edu.missionutils.Console;
 
 
 public class InputView {
-    private final InputValidator inputValidator;
 
-    public InputView() {
-        this.inputValidator = new InputValidator();
-    }
-
-    public int enterVisitDay() {
+    public String enterVisitDay() {
         String dayInput;
-        while (true) {
-            System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
-            System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-            try {
-                dayInput = Console.readLine();
-                int day = inputValidator.validateSingleNumber(dayInput);
-                return inputValidator.validateDayInRange(day);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage() + "\n");
-            }
-        }
+
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+
+        dayInput = Console.readLine();
+        return dayInput;
     }
-
-
 }
