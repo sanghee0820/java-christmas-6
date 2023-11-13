847d4f11a031d1fd7a0055a885ae8f74d73e140b
SangHee Lee <lsh277604@gmail.com>
2023-11-14 00:32:26 +0900

    Feat: run AppConsole

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,10 @@
 package christmas;
 
+import christmas.app.AppConsole;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConsole appConsole = new AppConsole();
+        appConsole.run();
     }
 }
