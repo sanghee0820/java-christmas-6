e8ce3578b12928f7497d2039fd642d397bbd4eb3
SangHee Lee <lsh277604@gmail.com>
2023-11-14 19:24:15 +0900

    Feat: add Order Class

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,2 +1,19 @@
-package christmas.domain;public class Order {
+package christmas.domain;
+
+public class Order {
+    private Food food;
+    private int quantity;
+
+    public Order(Food food, int quantity) {
+        this.food = food;
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "food=" + food +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
