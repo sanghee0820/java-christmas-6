b67d73e8bc897ef6282d006293554b9d269ff864
SangHee Lee <lsh277604@gmail.com>
2023-11-14 18:06:53 +0900

    Feat: add get food name from Repository

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,2 +1,20 @@
-package christmas.controller;public class ChristmasController {
+package christmas.controller;
+
+import christmas.service.DayService;
+import christmas.service.FoodService;
+import java.util.List;
+
+public class ChristmasController {
+    private final DayService dayService;
+    private final FoodService foodService;
+
+    public ChristmasController() {
+        this.dayService = new DayService();
+        this.foodService = new FoodService();
+    }
+
+    public List<String> getAllFood() {
+        return foodService.getAllFoodName();
+    }
+
 }
