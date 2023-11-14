c8143ac16900e06e71bbfdfaf9f1bc5a2da017fb
SangHee Lee <lsh277604@gmail.com>
2023-11-15 00:25:43 +0900

    Feat: get Event Info

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,20 +1,41 @@
 package christmas.controller;
 
+import christmas.DTO.EventDTO;
+import christmas.domain.Benefit;
+import christmas.domain.Day;
+import christmas.domain.Event;
+import christmas.domain.Order;
+import christmas.enumType.Badge;
+import christmas.service.BenefitService;
 import christmas.service.DayService;
 import christmas.service.FoodService;
+import java.util.HashMap;
 import java.util.List;
 
 public class ChristmasController {
     private final DayService dayService;
     private final FoodService foodService;
+    private final BenefitService benefitService;
 
     public ChristmasController() {
         this.dayService = new DayService();
         this.foodService = new FoodService();
+        this.benefitService = new BenefitService();
     }
 
     public List<String> getAllFood() {
         return foodService.getAllFoodName();
+    }
+
+    public EventDTO getEventInfo(int day, HashMap<String, Integer> orders) {
+        Day dayInfo = dayService.getDayInfo(day);
+        System.out.println("Day Info : " + dayInfo);
+        System.out.println("Order info = " + orders);
+        List<Order> orderInfo = foodService.setOrderInfo(orders);
+        List<Benefit> benefitInfo = benefitService.getBenefitInfo(dayInfo, orderInfo);
+        Badge badgeInfo = benefitService.getBadge(orderInfo);
+        Event christmasEvent = new Event(orderInfo, benefitInfo, badgeInfo);
+        return new EventDTO(christmasEvent);
     }
 
 }
