0201e14f2bdc4f4d09952d585e2a05bd522a5b49
SangHee Lee <lsh277604@gmail.com>
2023-11-14 16:45:05 +0900

    Feat: add Day class

--- src/main/java/christmas/domain/Day.java
+++ src/main/java/christmas/domain/Day.java
@@ -1,2 +1,16 @@
-package christmas.domain;public class Day {
+package christmas.domain;
+
+import christmas.enumType.Star;
+import christmas.enumType.Week;
+
+public class Day {
+    int day;
+    Star star;
+    Week week;
+
+    public Day(int day, Star star, Week week) {
+        this.day = day;
+        this.star = star;
+        this.week = week;
+    }
 }
