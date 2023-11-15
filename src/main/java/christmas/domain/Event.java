829ddb33a5c75787102c75d2e2910a70cb46eaf3
SangHee Lee <lsh277604@gmail.com>
2023-11-15 09:05:04 +0900

    Feat: add visitDay

--- src/main/java/christmas/domain/Event.java
+++ src/main/java/christmas/domain/Event.java
@@ -4,14 +4,20 @@ import christmas.enumType.Badge;
 import java.util.List;
 
 public class Event {
+
+    int visitDay;
     List<Order> orderInfo;
     List<Benefit> benefitInfo;
     Badge badge;
-
-    public Event(List<Order> orderInfo, List<Benefit> benefitInfo, Badge badge) {
+    public Event(int day, List<Order> orderInfo, List<Benefit> benefitInfo, Badge badge) {
+        this.visitDay = day;
         this.orderInfo = orderInfo;
         this.benefitInfo = benefitInfo;
         this.badge = badge;
+    }
+
+    public int getVisitDay() {
+        return visitDay;
     }
 
     public List<Order> getOrderInfo() {
