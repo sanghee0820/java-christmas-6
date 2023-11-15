181bdfae1480ed2419fba8b03d1630cf18284373
SangHee Lee <lsh277604@gmail.com>
2023-11-15 09:17:47 +0900

    Feat: add visitDay

--- src/main/java/christmas/DTO/EventDTO.java
+++ src/main/java/christmas/DTO/EventDTO.java
@@ -7,14 +7,37 @@ import java.util.HashMap;
 import java.util.List;
 
 public class EventDTO {
+    int totalPrice;
+    int visitDay;
     HashMap<String, Integer> orderInfo;
     HashMap<String, Integer> benefitInfo;
     String badge;
-
     public EventDTO(Event christmasEvent) {
         this.orderInfo = orderInfoToDTO(christmasEvent.getOrderInfo());
+        this.totalPrice = getTotalPrice(christmasEvent.getOrderInfo());
+        this.visitDay = christmasEvent.getVisitDay();
         this.benefitInfo = benefitInfoToDTO(christmasEvent.getBenefitInfo());
         this.badge = christmasEvent.getBadge().getDesc();
+    }
+
+    public int getVisitDay() {
+        return visitDay;
+    }
+
+    public HashMap<String, Integer> getOrderInfo() {
+        return orderInfo;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public HashMap<String, Integer> getBenefitInfo() {
+        return benefitInfo;
+    }
+
+    public String getBadge() {
+        return badge;
     }
 
     private HashMap<String, Integer> orderInfoToDTO(List<Order> orderInfo) {
@@ -23,6 +46,14 @@ public class EventDTO {
             orderInfoDTO.put(order.getFood().getName(), order.getQuantity());
         }
         return orderInfoDTO;
+    }
+
+    private int getTotalPrice(List<Order> orderInfo) {
+        int totalPrice = 0;
+        for (Order order : orderInfo) {
+            totalPrice += order.getFood().getPrice() * order.getQuantity();
+        }
+        return totalPrice;
     }
 
     private HashMap<String, Integer> benefitInfoToDTO(List<Benefit> benefitInfo) {
@@ -37,6 +68,7 @@ public class EventDTO {
     public String toString() {
         return "EventDTO{" +
                 "orderInfo=" + orderInfo +
+                ", totalPrice=" + totalPrice +
                 ", benefitInfo=" + benefitInfo +
                 ", badge='" + badge + '\'' +
                 '}';
